5168edc99a27f274e46f62fc945fe93fbf586f3d
NachoOrfei <nachoorfei12@gmail.com>
2024-10-24 10:27:48 -0300

    guardar proyecto/cargar proyecto/mostrar proyecto

--- tpCS/Program.cs
+++ tpCS/Program.cs
@@ -1,1 +1,71 @@
-﻿
+﻿using System.IO;
+using tpCS;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace tpCS
+{
+    class Program
+    {
+        static void Main()
+        {
+            SystemProyecto.CargarDatos();
+
+
+            int opcion;
+
+            do
+            {
+                Console.WriteLine("/////Bienvenido a TecnoInnovate/////");
+                Console.WriteLine("1.AGREGAR UN NUEVO PROYECTO");
+                Console.WriteLine("2.MOSTRAR TODOS LOS PROYECTOS");
+                Console.WriteLine("3.MODIFICAR PROYECTO EXISTENTE");
+                Console.WriteLine("4.ELIMINAR PROYECTO POR SU NOMBRE");
+                Console.WriteLine("5.GUARDAR PROYECTOS");
+                Console.WriteLine("6.SALIR");
+                opcion = int.Parse(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("Que Tipo de proyecto quiere agregar:\n" +
+                            "1.Proyecto de desarrollo Movil\n" +
+                            "2.Proyecto de Desarrollo  web");
+                        int pr = int.Parse(Console.ReadLine());
+                        switch (pr)
+                        {
+                            case 1:
+                                Menu.CrearProyecto_Movil();
+                                break;
+                            case 2:
+                                Menu.CrearProyecto_Web();
+
+                                break;
+                        }
+                        break;
+                    case 2:
+                        SystemProyecto.MostrarTodosLosProyectos();
+                        break;
+                    case 3:
+                        SystemProyecto.ModificarProyecto();
+                        break;
+                    case 4:
+                        SystemProyecto.EliminarProyecto();
+                        break;
+                    case 5:
+                        SystemProyecto.GuardarProyectos();
+                        break;
+                    case 6:
+
+                        break;
+
+
+
+                }
+            } while (opcion != 6);
+        }
+    }
+}
--- tpCS/sistema/SystemProyecto.cs
+++ tpCS/sistema/SystemProyecto.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using tpCS.ENUMS;
 
 namespace tpCS
 {
@@ -12,54 +13,160 @@ namespace tpCS
     {
         static List<Proyecto_Desarrollo_Movil> Desarrollo_Movil = new();
         static List<Proyecto_Desarrollo_Web> Desarrollo_Web = new();
+        static readonly string DesarrolloWeb = "Desarrollo_Web.txt";
+        static readonly string DesarrolloMovil = "Desarrollo_Movil.txt";
+        static public void AgregarProyectoMovil(Proyecto_Desarrollo_Movil desarrollo_Movil) => Desarrollo_Movil.Add(desarrollo_Movil);
+        static public void AgregarProyectoWeb(Proyecto_Desarrollo_Web desarrolloweb) => Desarrollo_Web.Add(desarrolloweb);
+        public static void GuardarProyectos()
+        {
+            using (StreamWriter writer = new StreamWriter(DesarrolloMovil))
+            {
+                foreach (var Proyecto in Desarrollo_Movil)
+                {
+                    writer.WriteLine($"{Proyecto.Nombre},{Proyecto.Plataforma},{Proyecto.CantidadDeDesarrolladores},{Proyecto.Estado},{Proyecto.FechaDeInicio},{Proyecto.FechaEstimada}");
+                }
+            }
 
+            using (StreamWriter writer = new StreamWriter(DesarrolloWeb))
+            {
+                foreach (var Proyecto in Desarrollo_Web)
+                {
+                    writer.WriteLine($"{Proyecto.Nombre},{Proyecto.Tecno},{Proyecto.CantidadDeDesarrolladores},{Proyecto.Estado},{Proyecto.FechaDeInicio},{Proyecto.FechaEstimada}");
+                }
+            }
+            Console.WriteLine("Proyectos guardado correctamente");
+        }
+
+        public static void CargarDatos()
+        {
+            if (File.Exists(DesarrolloMovil))
+            {
+                using (StreamReader reader = new StreamReader(DesarrolloMovil))
+                {
+                    string linea;
+                    while ((linea = reader.ReadLine()) != null)
+                    {
+
+                        var values = linea.Split(',');
+
+                        if (values.Length == 6)
+                        {
+                            string Nombre = values[0].Trim();
+                            Plataformas Plataforma = (Plataformas)Enum.Parse(typeof(Plataformas), values[1].Trim());
+                            int CantidadDeDesarrolladores = int.Parse(values[2].Trim());
+                            EstadoActual Estado = (EstadoActual)Enum.Parse(typeof(EstadoActual), values[3].Trim());
+                            DateTime FechaDeInicio = DateTime.Parse(values[4].Trim());
+                            DateTime FechaEstimada = DateTime.Parse(values[5].Trim());
+                            Desarrollo_Movil.Add(new Proyecto_Desarrollo_Movil(Nombre, Estado, CantidadDeDesarrolladores, Plataforma));
+                        }
+                    }
+                }
+                Console.WriteLine("Proyectos de desarrollo móvil cargados correctamente");
+            }
+            else
+            {
+                Console.WriteLine("No se encontró el archivo de proyectos móviles");
+            }
+
+            if (File.Exists(DesarrolloWeb))
+            {
+                using (StreamReader reader = new StreamReader(DesarrolloWeb))
+                {
+                    string linea;
+                    while ((linea = reader.ReadLine()) != null)
+                    {
+                        
+                        var values = linea.Split(',');
+
+                        if (values.Length == 6)
+                        {
+                            string Nombre = values[0].Trim();
+                            Tecnologias Tecno = (Tecnologias)Enum.Parse(typeof(Tecnologias), values[1].Trim());
+                            int CantidadDeDesarrolladores = int.Parse(values[2].Trim());
+                            EstadoActual Estado = (EstadoActual)Enum.Parse(typeof(EstadoActual), values[3].Trim());
+                            DateTime FechaDeInicio = DateTime.Parse(values[4].Trim());
+                            DateTime FechaEstimada = DateTime.Parse(values[5].Trim());
+                            Desarrollo_Web.Add(new Proyecto_Desarrollo_Web(Nombre, Estado, CantidadDeDesarrolladores, Tecno));
+                        }
+                    }
+                }
+                Console.WriteLine("Proyectos de desarrollo web cargados correctamente");
+            }
+            else
+            {
+                Console.WriteLine("No se encontró el archivo de proyectos web");
+            }
+        }
+
+        public static void MostrarTodosLosProyectos()
+        {
+            Console.WriteLine("-----PROYECTOS DESARROLLO WEB-----\n");
+            foreach (var proyectoweb in Desarrollo_Web)
+            {
+                Console.WriteLine(proyectoweb.ToString());
+            }
+            Console.WriteLine("\n----PROYECTOS DESARROLLO MOVIL-----\n");
+            foreach (var proyectomovil in Desarrollo_Movil)
+            {
+                Console.WriteLine(proyectomovil.ToString());
+            }
+        }
         public static void EliminarProyecto()
         {
             int opcion;
-            Console.WriteLine(" que tipo de proyecto desea eliminar");
+            Console.WriteLine("¿que tipo de proyecto desea eliminar?" +
+                              "\n1. Desarrollo Movil\n" +
+                              "2. Desarrollo Web");
             opcion = int.Parse(Console.ReadLine());
+
             switch (opcion)
             {
                 case 1:
+                    
                     foreach (var proyectoMovil in Desarrollo_Movil)
                     {
-                        Console.WriteLine($"nombre:{proyectoMovil.Nombre}");
+                        Console.WriteLine($"Nombre: {proyectoMovil.Nombre}");
                     }
-                    Console.WriteLine("ingrese el nombre del proyecto que desea eliminar");
-                    string nombre=Console.ReadLine();
-                    var proyectoAEliminar = Desarrollo_Movil.Find(proyecto => proyecto.Nombre.Equals(nombre, StringComparison.OrdinalIgnoreCase));
+                    Console.WriteLine("Ingrese el nombre del proyecto que desea eliminar:");
+                    string nombreMovil = Console.ReadLine();
+                    var proyectoAEliminarMovil = Desarrollo_Movil.Find(proyecto => proyecto.Nombre.Equals(nombreMovil, StringComparison.OrdinalIgnoreCase));
 
-                    if (proyectoAEliminar != null)
+                    if (proyectoAEliminarMovil != null)
                     {
-                        Desarrollo_Movil.Remove(proyectoAEliminar);
-                        Console.WriteLine($"Proyecto :{nombre}, eliminado con exito.");
+                        Desarrollo_Movil.Remove(proyectoAEliminarMovil);
+                        Console.WriteLine($"Proyecto: {nombreMovil} eliminado con exito.");
                     }
                     else
                     {
-                        Console.WriteLine($"Proyecto :{nombre}, no encontrado.");
+                        Console.WriteLine($"Proyecto: {nombreMovil} no encontrado.");
                     }
                     break;
-                case 2:
-                    foreach (var proyectoMovil in Desarrollo_Movil)
-                    {
-                        Console.WriteLine($"nombre:{proyectoMovil.Nombre}");
-                    }
-                    Console.WriteLine("ingrese el nombre del proyecto que desea eliminar");
-                    string nombreMovil = Console.ReadLine();
-                    var proyectoAeliminar = Desarrollo_Web.Find(proyecto => proyecto.Nombre.Equals(nombreMovil, StringComparison.OrdinalIgnoreCase));
 
-                    if (proyectoAeliminar != null)
+                case 2:
+                   
+                    foreach (var proyectoWeb in Desarrollo_Web)
                     {
-                        Desarrollo_Web.Remove(proyectoAeliminar);
-                        Console.WriteLine($"Proyecto :{nombreMovil}, eliminado con exito.");
+                        Console.WriteLine($"Nombre: {proyectoWeb.Nombre}");
+                    }
+                    Console.WriteLine("Ingrese el nombre del proyecto que desea eliminar:");
+                    string nombreWeb = Console.ReadLine();
+                    var proyectoAEliminarWeb = Desarrollo_Web.Find(proyecto => proyecto.Nombre.Equals(nombreWeb, StringComparison.OrdinalIgnoreCase));
+
+                    if (proyectoAEliminarWeb != null)
+                    {
+                        Desarrollo_Web.Remove(proyectoAEliminarWeb);
+                        Console.WriteLine($"Proyecto: {nombreWeb} eliminado con exito.");
                     }
                     else
                     {
-                        Console.WriteLine($"Proyecto: {nombreMovil}, no encontrado.");
+                        Console.WriteLine($"Proyecto: {nombreWeb} no encontrado.");
                     }
+                    break;
+
+                default:
+                    Console.WriteLine("Opcion no valida.");
                     break;
             }
-
         }
         public static void ModificarProyecto()
         {
